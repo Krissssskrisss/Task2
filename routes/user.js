deca56adc91cc0391f6f5286eb808ca3a78d5d6d
soltant <taras.soltan.92@gmail.com>
2017-07-25 18:04:20 +0300

    added post put and delete

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hi user');
+  res.send('Hi get');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('Hi post');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('Hi put');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('Hi delete');
 });
 
 module.exports = router;
