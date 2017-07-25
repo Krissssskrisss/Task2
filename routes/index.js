0a8c8119bf7e146c041e8cbb374782b4669324ee
soltant <taras.soltan.92@gmail.com>
2017-07-25 17:18:47 +0300

    fixed indents

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('index.js', { root: './views' });
+  res.sendFile('index.js', { root: './views' });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Hi user');
+  res.send('Hi user');
 });
 
 module.exports = router;
