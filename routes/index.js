8fe69520f7dab220a87a5fad61cb09f85a21ad3e
soltant <taras.soltan.92@gmail.com>
2017-07-25 15:19:19 +0300

    setuped index.js for /

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("test");
+    res.sendFile('index.js', { root: './views' });
 });
 
 module.exports = router;
